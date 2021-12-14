b2415c9bc96cc35e452534e5b5d2808428047eb7
faraz ahmad siddiqui <faraz841@gmail.com>
2021-12-14 16:33:04 +0530

    code base updated

---
+++ src/Database/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DbUp;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false);
+
+var config = builder.Build();
+
+var connectionString = args.FirstOrDefault() ?? config["ConnectionString"];
+
+var upgrader = DeployChanges.To
+                 .SqlDatabase(connectionString)
+                 .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                 .LogToConsole()
+                 .Build();
+
+var result = upgrader.PerformUpgrade();
+if (!result.Successful)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(result.Error);
+    Console.ResetColor();
+
+#if DEBUG
+    Console.ReadLine();
+#endif
+
+    return -1;
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Success!");
+Console.ResetColor();
+return 0;
---
+++ src/Database/Scripts/tbl_Contract.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tbl_Contract](
+	[ContractId] [varchar](50) NOT NULL,
+	[CreatedDate] [datetime] NOT NULL,
+	[EndDate] [datetime] NOT NULL,
+	[UpdateDatetime] [datetime] NOT NULL,
+	[Price] [decimal](18, 2) NOT NULL,
+	[Owner] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_tbl_Contract] PRIMARY KEY CLUSTERED 
+(
+	[ContractId] ASC,
+	[CreatedDate] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
